8f5436ad69c9da9b208c2ca9e648bc6502586fc8
Talitha Souza <litha06h@gmail.com>
2020-04-01 19:29:41 -0300

    testando adicionar arquivos criados no sublime

---
+++ sub.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste do sublime</title>
+</head>
+<body>
+	<CENTER>
+		<h1>testando aqui enviar arquivo feitos no sublime</h1>	<br>
+	</CENTER>
+	
+</body>
+</html>
